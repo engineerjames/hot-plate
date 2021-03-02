0280f3df2ff06504ca8e37097adb7cd586f8c0f5
James Armes <jamesleearmes@gmail.com>
2021-03-01 21:10:06 -0700

    Fixed the worst of the math errors--never trust a textbook that does not include parantheses

--- src/example_1/analytic_solution/solution.m
+++ src/example_1/analytic_solution/solution.m
@@ -23,14 +23,14 @@
 %    0               a
 
 % We'll prescribe T1 = 300K, T2 = 400K, T3 = 50K, and T4 = 75K
-T1 = 300
-T2 = 400
-T3 = 50
-T4 = 75
+T1 = 200;
+T2 = 500;
+T3 = 200;
+T4 = 200;
 
 % We'll have a plate that is 1.5m x 3m
-a = 1.5
-b = 3.0
+a = 1.5;
+b = 3.0;
 
 function coefficients = getCoefficients(n, T1, T2, T3, T4, a, b)
 %getCoefficients - Description
@@ -38,27 +38,56 @@ function coefficients = getCoefficients(n, T1, T2, T3, T4, a, b)
 % Syntax: [A B C D] = getCoefficients(n)
 %
 % Long description
-    An = (T1 * sin((n * pi)/a) * a) / sinh((n * pi * b)/a);
-    Bn = (T2 * sin((n * pi)/a) * a) / sinh((n * pi * b)/a);
-    Cn = (T3 * sin((n * pi)/b) * b) / sinh((n * pi * a)/b);
-    Dn = (T4 * sin((n * pi)/b) * b) / sinh((n * pi * a)/b);
+    nPiOverA = (n * pi)/a;
+    nPiOverB = (n * pi)/b;
+
+    nPiAOverB = (n*pi*a) / b;
+    nPiBOverA = (n*pi*b) / a;
+
+    % Old calculations based on misinterpretation of sin term
+    %An = (T1 * sin(nPiOverA) * a) / sinh(nPiBOverA);
+    %Bn = (T2 * sin(nPiOverA) * a) / sinh(nPiBOverA);
+    %Cn = (T3 * sin(nPiOverB) * b) / sinh(nPiAOverB);
+    %Dn = (T4 * sin(nPiOverB) * b) / sinh(nPiAOverB);
+    %An = ( 2 * T1 * (1 - cos(pi*n))) / (pi * n * sinh(nPiBOverA));
+    %Bn = ( 2 * T2 * (1 - cos(pi*n))) / (pi * n * sinh(nPiBOverA));
+    An = (2 * T1 * (a - a * cos(pi*n)) * csch(nPiBOverA)) / (pi * a * n);
+    Bn = (2 * T2 * (a - a * cos(pi*n)) * csch(nPiBOverA)) / (pi * a * n);
+    Cn = (2 * T3 * (b - b * cos(pi*n)) * csch(nPiAOverB)) / (pi * b * n);
+    Dn = (2 * T4 * (b - b * cos(pi*n)) * csch(nPiAOverB)) / (pi * b * n);
 
     coefficients = [An Bn Cn Dn];
 end
 
 % There are also four distinct infinite sum
 % for a given (x, y) coordinate
-for n = 1:100
-    coeff = getCoefficients(n, T1, T2, T3, T4, a, b)
 
-    % Just for fun let's find the mid-point temperature
-    x = a/2
-    y = b/2
+X_STEPS = 100;
+Y_STEPS = 100;
 
-    term1 = coeff(1) * sin((n*pi)/a) * x * sinh((n*pi)/a) * (b - y);
-    term2 = coeff(2) * sin((n*pi)/a) * x * sinh((n*pi)/a) * y;
-    term3 = coeff(3) * sinh((n*pi)/b) * (a-x) * sin((n*pi)/b) * y;
-    term4 = coeff(4) * sinh((n*pi)/b) * x * sin((n*pi)/b) * y;
+for x = 0:a/X_STEPS:a
+    for y = 0:b/Y_STEPS:b
 
-    theSum(n) = term1 + term2 + term3 + term4;
+        %x = 0.75;
+        %y = 0.00;
+        printf("Getting value for x=%1.3f, y=%1.3f\n", x, y);
+
+        for n = 1:100
+            coeff = getCoefficients(n, T1, T2, T3, T4, a, b);
+
+            nPiOverA = (n * pi) / a;
+            nPiOverB = (n * pi) / b;
+
+            term1(n) = coeff(1) * sin(nPiOverA * x) * sinh(nPiOverA * (b - y));
+            term2(n) = coeff(2) * sin(nPiOverA * x) * sinh(nPiOverA * y);
+            term3(n) = coeff(3) * sinh(nPiOverB * (a-x)) * sin(nPiOverB * y);
+            term4(n) = coeff(4) * sinh(nPiOverB * x) * sin(nPiOverB * y);
+        end
+
+        theSum = term1 + term2 + term3 + term4;
+        printf("Populating index (%i, %i).\n", int32(x/a * X_STEPS), int32(y/b * Y_STEPS));
+        u(int32(x/a * X_STEPS) + 1, int32(y/b * Y_STEPS) + 1) = sum(theSum);
+
+    end
 end
+surf(0:a/X_STEPS:a, 0:b/Y_STEPS:b, u)
