ccefb995c307620d548bb02f02f38e5fa6c929bd
James Armes <jamesleearmes@gmail.com>
2021-03-06 22:06:53 -0700

    Added clarifying comment.

--- src/example_1/RandomWalkerSimulation.hpp
+++ src/example_1/RandomWalkerSimulation.hpp
@@ -3,6 +3,11 @@
 
 #include "Parameters.hpp"
 
+// Initially, the intent is to just have this class manage the execution of
+// a single strategy--we'll iterate through each discrete point in the domain
+// and generate N walks at each point to establish the temperature distribution.
+// In the future, we'll likely inject the specific walking strategy so that various
+// computational approaches can be explored (Heavy parallelization, GPU acceleration, etc.)
 class RandomWalkerSimulation
 {
   public:
